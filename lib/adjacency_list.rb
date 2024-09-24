3866725e25972cfb1cf00248de0c50124b8329d4
Yoni190 <yfisuhu@gmail.com>
2024-09-24 15:18:42 +0300

    Create methods to add squares and add moves

--- lib/adjacency_list.rb
+++ lib/adjacency_list.rb
@@ -7,15 +7,20 @@ class AdjacencyList
         self.adjacency_array = []
     end
 
-    def addNode(value)
+    def add_node(value)
         list = LinkedList.new
         list.append(value)
         self.adjacency_array.push(list)
     end
 
-    def addEdge(src, dst)
-        srcList = self.adjacency_array[src]
-        dstList = self.adjacency_array[dst]
+    def add_edge(src, dst)
+        values = adjacency_array.map do |element|
+            element.head.value
+        end
+        src_index = values.find_index(src)
+        dst_index = values.find_index(dst)
+        srcList = adjacency_array[src_index]
+        dstList = adjacency_array[dst_index]
         srcList.append(dstList.head.value)
         dstList.append(srcList.head.value)
     end
--- main.rb
+++ main.rb
@@ -2,19 +2,59 @@ require_relative 'lib/adjacency_list'
 
 ad_list = AdjacencyList.new
 
-ad_list.addNode("A")
-ad_list.addNode("B")
-ad_list.addNode("C")
-ad_list.addNode("D")
-ad_list.addNode("E")
 
-ad_list.addEdge(0, 1)
-ad_list.addEdge(1, 2)
-ad_list.addEdge(1, 4)
-ad_list.addEdge(2, 3)
-ad_list.addEdge(2, 4)
-ad_list.addEdge(4, 0)
-ad_list.addEdge(4, 2)
+def add_squares(ad_list)
+    x = 0
+    y = 0
+    while x < 8
+        while y < 8
+            ad_list.add_node("[#{x}, #{y}]")
+            y += 1
+        end
+        x += 1
+        y = 0
+    end
+end
+
+def add_moves(ad_list)
+    x = 0
+    y = 0
+    while x < 8
+        while y < 8
+            if x + 2 < 8 && y + 1 < 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x + 2}, #{y + 1}]")
+            end
+            if x + 1 < 8 && y + 2 < 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x + 1}, #{y + 2}]")
+            end
+            if x + 2 < 8 && y -1 > 0
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x + 2}, #{y - 1}]")
+            end
+            if x + 1 < 8 && y - 2 > 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x + 1}, #{y - 2}]")
+            end
+            if x - 2 > 8 && y + 1 < 1
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x - 2}, #{y + 1}]")
+            end
+            if x - 1 > 8 && y + 2 < 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x - 1}, #{y + 2}]")
+            end
+            if x -2 > 8 && y -1 > 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x - 2}, #{y - 1}]")
+            end
+            if x - 1 > 8 && y - 2 > 8
+                ad_list.add_edge("[#{x}, #{y}]", "[#{x - 1}, #{y - 2}]")
+            end
+            y += 1
+        end
+        x += 1
+        y = 0
+    end
+end
+
+
+add_squares(ad_list)
+add_moves(ad_list)
 
 #ad_list.to_s
 puts ad_list
